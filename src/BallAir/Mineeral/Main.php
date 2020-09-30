2a2549a2ef4b2d5c6672080d68afccdb8e1ebf16
MIneeral <63883918+MIneeral@users.noreply.github.com>
2020-09-30 23:56:12 +0200

    Update Main.php

--- src/BallAir/Mineeral/Main.php
+++ src/BallAir/Mineeral/Main.php
@@ -1,1 +1,24 @@
+<?php 
 
+namespace BallAir\Mineeral;
+
+use BallAir\Mineeral\Event\EventListener;
+
+use pocketmine\utils\Config;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+    public function onEnable()
+    {
+        @mkdir($this->getDataFolder());
+        
+        $this->getServer->getPluginManager->registerEvents(new EventListener($this), $this);
+
+
+        if(!file_exists($this->getDataFolder() . "settings.yml")){
+        $this->saveResource('settings.yml');
+        }
+    }
+}
